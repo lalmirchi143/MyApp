5ce6a7cc2a92acc57d803354f81688fad4febbd5
USER <USER@192.168.0.6>
2019-05-09 21:50:06 +0530

    hello

---
+++ src/com/app/Admin/Admin.java
@@ -0,0 +1,20 @@
+package com.app.Admin;
+
+public class Admin {
+	private Integer adminId;
+
+	public Admin(Integer adminId) {
+		super();
+		this.adminId = adminId;
+	}
+
+	public Integer getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(Integer adminId) {
+		this.adminId = adminId;
+	}
+	
+
+}
---
+++ src/com/app/Emp/Emp.java
@@ -0,0 +1,20 @@
+package com.app.Emp;
+
+public class Emp {
+	private Integer empId;
+
+	public Emp(Integer empId) {
+		super();
+		this.empId = empId;
+	}
+
+	public Integer getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+	
+
+}
